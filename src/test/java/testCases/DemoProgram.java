9f12ee86763b9ae7ff770209748339ffc7b6e37a
Raushan Kumar <vicky.raushan@gmail.com>
2022-02-06 13:23:59 +0530

    Added browser parameters

--- src/test/java/testCases/DemoProgram.java
+++ src/test/java/testCases/DemoProgram.java
@@ -3,17 +3,30 @@ package testCases;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class DemoProgram {
 
     WebDriver driver;
+    @Parameters("Browser")
     @Test
-    public void demo_startBrowser()
+    public void demo_startBrowser(String browserName)
     {
-        WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
+        System.out.println("Parameter value is -> "+browserName);
+        if(browserName.equalsIgnoreCase("chrome"))
+        {
+            WebDriverManager.chromedriver().setup();
+            driver=new ChromeDriver();
+        }
+        else if(browserName.equalsIgnoreCase("edge"))
+        {
+            WebDriverManager.edgedriver().setup();
+            driver=new EdgeDriver();
+        }
+
         driver.manage().window().maximize();
         driver.get("https://www.google.co.in");
         Assert.assertTrue(driver.getTitle().equals("Google"),"Page did not open");
